c25701447084fb9747107adb63305305196f6d09
Russ Goetz <russgoetz@gmail.com>
2023-07-29 13:26:29 -0400

    Initial addition of ReplaceNameInComments.java script

---
+++ ReplaceNameInComments.java
@@ -0,0 +1,16 @@
+//Replaces a variable name in comments with a new name.
+//@author Russ Goetz
+//@category 
+//@keybinding
+//@menupath
+//@toolbar
+
+import ghidra.app.script.GhidraScript;
+
+public class ReplaceNameInComments extends GhidraScript {
+
+	@Override
+	protected void run() throws Exception {
+		//TODO: Add script code here
+	}
+}
